2632105f392915d71d6313c53e417161b663ed30
Shane Cheek <unitedideas@gmail.com>
2019-01-15 12:50:32 -0800

    lab2 start

---
+++ programming/lab2.py
@@ -0,0 +1,18 @@
+__author__ = "Shane Cheek"
+
+#inputs:
+    # Formula for converting Fahrenheit to Celsius ((Fahrenheit − 32) × 5/9 = Celsius)
+    # Formula for converting Celsius to Kelvin (Celsius + 273.15 = Kelvin)
+
+# display instructions options "C, K or F" for Celsius, Kelvin or Fahrenheit
+# ask for input conversion from "C, K or F"
+# ask for input conversion to "C, K or F"
+# display input1 + " = " calculation_output + input2
+
+print('we will convert from / to Fahrenheit, Celsius or Kelvin')
+print("select ")
+
+
+
+
+
