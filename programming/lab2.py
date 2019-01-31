718ac7240e35d76bba324b69bc080c10972b58fb
Shane Cheek <unitedideas@gmail.com>
2019-01-31 11:27:17 -0800

    Dumbed down for the class Removed error checking

--- programming/lab2.py
+++ programming/lab2.py
@@ -1,85 +1,73 @@
 __author__ = "Shane Cheek"
 
 # inputs:
-# Formula for converting Fahrenheit to Celsius  (f − 32) × 5/9 = converted
-# Formula for converting Fahrenheit to Kelvin   (f - 32) * 5/9 + 273.15 = converted
-# Formula for converting Celsius to Fahrenheit  (c * 9/5) + 32 = converted
-# Formula for converting Celsius to Kelvin      c + 273.15 = converted
-# Formula for converting Kelvin to Celsius      k - 273.15 = converted
-# Formula for converting Kelvin to Fahrenheit   (K - 273.15) * 9/5 + 32 = converted
-# unit_options c,f,k
-
-# process:
-# calculation
-
 # output:
-# calculation
+# Display "str(qty_from) + " " + str(unit_from) + " = " + str(conversion) + " " + str(unit_to)"
 
 
-# pseudocode:
-# if do_again is true
-# create options
-"""display instructions options "C, K or F" for Celsius, Kelvin or Fahrenheit
-and ask for input conversion from "C, K or F"""
-# get from quantity
-# check if C,F or K were inputs
-# remove selection from options
-# ask for input conversion to "C, K or F", but not the same as previously selected
-# check if input is an option remaining in unit_options
-# calculate conversion
-# display input1 + " = " calculation_output + input2
-# ask if they want to do it again
-# bye bye
+# Declare List unit_options
+# Declare String unit_from
+# Declare Real qty_from
+# Declare String unit_to
+# Declare Dict formulas
+# Declare Real conversion
+#
+# Set unit_options
+#
+# Display 'We will convert between Fahrenheit, Celsius or Kelvin. \n'
+#     'Please select the unit you want to convert from. \n'
+#     'Type f for Fahrenheit, c for Celsius or k for Kelvin:\n'
+#
+# Input unit_from
+#
+# Remove unit_from from unit_options
+#
+# Display Enter a quantity. \n
+#
+# Input qty_from
+#
+# Display 'Please select the unit you want to convert to, but not the same as converting from.  \n'
+#     'Please choose ' + unit_options[0] + ' or ' + unit_options[1] + '\n'
+#
+# Input unit_to
+#
+# Set conversion = formulas[unit_from][unit_to]
+#
+# Display str(qty_from) + " " + str(unit_from) + " = " + str(conversion) + " " + str(unit_to)
 
-do_again = "y"
 
-while do_again == "y":
-    unit_options = ["c", 'f', 'k']
 
-    unit_from = input(
-        'We will convert between Fahrenheit, Celsius or Kelvin. \n'
-        'Please select the unit you want to convert from. \n'
-        'Type f for Fahrenheit, c for Celsius or k for Kelvin:\n').lower()
 
-    if unit_from not in unit_options:
-        while unit_from not in unit_options:
-            unit_from = input(
-                str(unit_from) + ' is not an option please choose ' + unit_options[0] + ' or ' + unit_options[
-                    1] + ' or ' +
-                unit_options[2] + '\n').lower()
 
-    unit_options.remove(unit_from)
+unit_options = []
+unit_from = ''
+qty_from = 0.0
+unit_to = ''
+formulas = {}
+conversion = 0.0
 
-    while True:
-        try:
-            qty_from = float(input('Enter a quantity. \n'))
-            break
-        except ValueError:
-            print("That is not a number.")
+unit_options = ["c", 'f', 'k']
 
-    unit_to = input(
-        'Please select the unit you want to convert to, but not the same as converting from.  \n'
-        'Please choose ' + unit_options[0] + ' or ' + unit_options[1] + '\n').lower()
+unit_from = input(
+    'We will convert between Fahrenheit, Celsius or Kelvin. \n'
+    'Please select the unit you want to convert from. \n'
+    'Type f for Fahrenheit, c for Celsius or k for Kelvin:\n').lower()
 
-    if unit_to not in unit_options:
-        while unit_to not in unit_options:
-            unit_to = input(
-                str(unit_to) + ' is not an option please choose ' + unit_options[0] + ' or ' + unit_options[
-                    1] + '\n').lower()
+unit_options.remove(unit_from)
 
-    formulas = {
-        "c": {"f": qty_from * (9 / 5) + 32, "k": qty_from + 273.15},
-        "f": {"c": (qty_from - 32) * (5 / 9), "k": (qty_from - 32) * 5 / 9 + 273.15},
-        'k': {"c": qty_from - 273.15, "f": qty_from + 273.15}}
+qty_from = float(input('Enter a quantity. \n'))
 
-    conversion = formulas[unit_from][unit_to]
-    print(conversion)
-    print(str(qty_from) + " " + str(unit_from) + " = " + str(conversion) + " " + str(unit_to))
+unit_to = input(
+    'Please select the unit you want to convert to, but not the same as converting from.  \n'
+    'Please choose ' + unit_options[0] + ' or ' + unit_options[1] + '\n').lower()
 
-    do_again = input('Do another? y = yes, n = no\n').lower()
+formulas = {
+    "c": {"f": qty_from * (9 / 5) + 32, "k": qty_from + 273.15},
+    "f": {"c": (qty_from - 32) * (5 / 9), "k": (qty_from - 32) * 5 / 9 + 273.15},
+    'k': {"c": qty_from - 273.15, "f": qty_from + 273.15}}
 
-    if do_again != "y" and do_again != "n":
-        while do_again != "y" and do_again != "n":
-            do_again = input('Do another? y = yes, n = no\n').lower()
+conversion = formulas[unit_from][unit_to]
 
-print("Bye Bye")
+print(str(qty_from) + " " + str(unit_from) + " = " + str(conversion) + " " + str(unit_to))
+
+
