e3b802d8e77d12c1f9d240f34ed4340ce2bb3603
Shane Cheek <unitedideas@gmail.com>
2019-02-14 17:15:18 -0800

    init

---
+++ intro_to_programming/lab4.py
@@ -0,0 +1,16 @@
+__author__ = 'Shane Cheek'
+
+
+# description
+
+# input
+
+# output
+
+# pseudocode
+
+import math
+
+for num in str(math.pi):
+    if num != ".":
+        print(num)
---
+++ intro_to_programming/lab_1_hello.py
@@ -0,0 +1,10 @@
+__author__ = "Shane Cheek"
+
+# display "Hello World"
+
+say = "Hello World"
+print(say)
+
+# or
+
+print("Hello World")
