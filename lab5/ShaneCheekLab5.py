2501c333e2af2f4f54f87cf62de39fd310340b7f
Shane Cheek <unitedideas@gmail.com>
2019-02-28 18:12:34 -0800

    Computer Logic Lab5

--- lab5/ShaneCheekLab5.py
+++ lab5/ShaneCheekLab5.py
@@ -2,14 +2,17 @@ import time
 
 start = time.time()
 
-name_list = []
 
 name = input('name')
 
 sr = list(name)
 
+totName = 0
 for character in sr:
+    totName+=ord(character)
     print(ord(character))
+print(totName)
+print(chr(totName))
 
 stop = time.time()
 
