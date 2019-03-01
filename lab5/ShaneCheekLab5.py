2781c4bc0e2b2b33160f758079424d447a0e1037
Shane Cheek <unitedideas@gmail.com>
2019-02-28 21:24:37 -0800

    making letters into art

--- lab5/ShaneCheekLab5.py
+++ lab5/ShaneCheekLab5.py
@@ -3,44 +3,47 @@ import random
 
 from turtle import *
 
-start = round(time.time()/(60*60*24*365)*2)
 
-print(start)
-name = input('first name')
-name += ' '
-name += input('last name')
+name = input('First Name: ')
+name += " "
+name += input('Last Name: ')
 
-stop = round(time.time()/(60*60*24*365)*2)
-totalTime = round(stop - start)
+name_to_char_list = list(name.split(' '))
 
-sr = list(name.split(' '))
+names_in_2d_list = []
 
-full_list = []
+for name in name_to_char_list:
+    names_in_2d_list.append(list(name))
 
-for name in sr:
-    full_list.append(list(name))
-
-print(full_list)
+print(names_in_2d_list)
 
 totName = 0
 
-for name in full_list:
+for name in names_in_2d_list:
     for character in name:
         totName += ord(character)
         print(ord(character))
 print(totName)
 print(chr(totName))
 
-
-
-color('red', 'yellow')
-begin_fill()
-for name in full_list:
-    forward(start)
+joe = Turtle()
+joe.hideturtle()
+joe.speed(1000)
+joe.color('red', 'yellow')
+start_marker_y = 100
+window_width = 350
+for name in names_in_2d_list:
+    start_marker_x = (window_width / len(name)) - window_width
+    joe.penup()
+    joe.goto(start_marker_x, start_marker_y)
+    joe.pendown()
     for character in name:
-        forward(stop)
-        left(ord(character)*2)
-end_fill()
-done()
-print('done')
-
+        joe.penup()
+        joe.goto(start_marker_x, start_marker_y)
+        joe.pendown()
+        for count in range(ord(character)):
+            joe.forward(count)
+            joe.left(ord(character))
+        start_marker_x += (690 / len(name))
+    start_marker_y += -200
+time.sleep(13.5)
