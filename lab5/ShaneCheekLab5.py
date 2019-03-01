6d214d42b0c6dab6c9019d695918a66e78d93fdb
Shane Cheek <unitedideas@gmail.com>
2019-02-28 18:48:57 -0800

    Computer Logic Lab5

--- lab5/ShaneCheekLab5.py
+++ lab5/ShaneCheekLab5.py
@@ -1,19 +1,46 @@
 import time
+import random
 
-start = time.time()
+from turtle import *
 
+start = round(time.time()/(60*60*24*365)*2)
 
-name = input('name')
+print(start)
+name = input('first name')
+name += ' '
+name += input('last name')
 
-sr = list(name)
+stop = round(time.time()/(60*60*24*365)*2)
+totalTime = round(stop - start)
+
+sr = list(name.split(' '))
+
+full_list = []
+
+for name in sr:
+    full_list.append(list(name))
+
+print(full_list)
 
 totName = 0
-for character in sr:
-    totName+=ord(character)
-    print(ord(character))
+
+for name in full_list:
+    for character in name:
+        totName += ord(character)
+        print(ord(character))
 print(totName)
 print(chr(totName))
 
-stop = time.time()
 
-print(round(stop - start, 2))
+
+color('red', 'yellow')
+begin_fill()
+for name in full_list:
+    forward(start)
+    for character in name:
+        forward(stop)
+        left(ord(character)*2)
+end_fill()
+done()
+print('done')
+
