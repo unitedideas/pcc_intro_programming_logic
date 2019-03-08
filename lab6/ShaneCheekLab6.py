719cf338029ad6fa6bf59151e642dc7ba22be6de
Unitedideas <unitedideas@gmail.com>
2019-03-08 12:03:17 -0800

    Building an oop dog park.

--- lab6/ShaneCheekLab6.py
+++ lab6/ShaneCheekLab6.py
@@ -1,6 +1,5 @@
 __author__ = "Shane Cheek"
 
-
 # Description:
 #     This program will represent a dog park. It will allow
 #     dogs to come and go in and out of the dog park. New
@@ -14,59 +13,101 @@ __author__ = "Shane Cheek"
 #     This will instantiate a new dog or destroy a instance
 #     of the dog named by the user (input).
 
+
 # Output:
+
+options = {
+    'add': 'Add a dog to the dog park.',
+    'remove': 'Remove a dog from the dog park.',
+    'print': 'Print all dogs in the dog park.',
+    'quit': 'Quit the program',
+    'owner': 'Check the owner of a dog'
+}
+
+dogInstanceList = {}
 
 
 class Dog:
-    def __init__(self, _name, _age, _owner):
-        self._name = _name
-        self._age = _age
-        self._owner = _owner
+    # def __init__(self, name='unknown', owner='unknown'):
+    #     self.__name = name
+    #     self.__owner = owner
 
-    def __del__(self):
-        print('Removed', self._name, 'from the dog park. Bye Bye', self._name)
+    def setter(self, name, owner='unknown'):
+        self.__name = name
+        self.__owner = owner
+
+    def dog_name_getter(self):
+        return self.__name
+
+    def owner_name_getter(self):
+        return self.__owner
 
 
-dogInstanceList = ['shane']
+def handle_dog_instance_list(name):
+    dogInstanceList[name] = name
+    print(name, dogInstanceList[name, name])
 
 
-def handleDogInstanceList(name):
-    dogInstanceList.append(name)
+def print_dog():
+    if len(dogInstanceList) > 0:
+        print('The dog/s in the dog park are:')
+        for dog in dogInstanceList:
+            print(dogInstanceList[dog].dog_name_getter().title())
+        return True
+    else:
+        print('There are no dogs in the dog park.')
+        return False
 
 
-# def set_name(self):
-#     self._name = _name
+def add_dog():
+    dog_name = input('Name of the dog. ').lower()
+    if dog_name in dogInstanceList:
+        print('This dog is already in the dog park.')
+    else:
+        new_dog = Dog()
+        new_dog.setter(dog_name, input('Owner of the dog. ', ).lower())
+        dogInstanceList[dog_name] = new_dog
+        print(dog_name.title(), 'has entered the dog park. Have fun buddy!')
 
-# def handleQuitCommand(command):
-#     if command == 'quit':
-#         return break
 
-options = ['add', 'remove', 'print', 'quit']
+def remove_dog():
+    if print_dog():
+        name = input('Name the dog you want to remove from the park. ')
+        try:
+            del dogInstanceList[name]
+            print(name.title(), 'has been removed.')
+            print_dog()
+        except ValueError:
+            print(str(name).title(), 'is not in the dog park ')
+
+
+def dog_owner():
+    if print_dog():
+        dog_name = input('Name the dog you want to know the owner of. ')
+        try:
+            print(dog_name + '\'s', 'owner is', dogInstanceList[dog_name].owner_name_getter())
+        except KeyError:
+            print(str(dog_name).title(), 'is not in the dog park ')
+
+
+def command_option():
+    return input('What would you like to do? Add, Remove, Print, Quit, Owner ').lower()
+
 
 while True:
-    command = input('What would you like to do? Add, Remove, Print, Quit ').lower()
+    command = command_option()
 
     if command in options:
         if command == 'quit':
             break
         elif command == 'print':
-            if len(dogInstanceList) > 0:
-                for dog in dogInstanceList:
-                    print(dog)
-            else:
-                print('There are no dogs in the dog park.')
+            print_dog()
 
         elif command == 'add':
-            name = input('name').lower()
-            name = Dog(name, input('age').lower())
-            print(name._name, name._age)
-            handleDogInstanceList(name._name)
+            add_dog()
 
         elif command == 'remove':
-            name = input('Name the dog you want to remove from the park. ')
-            try:
-                del locals()[name]
-                dogInstanceList.remove(name)
-                print('Remaining Dog ' + str(dogInstanceList))
-            except KeyError:
-                print(str(name).title(), 'is not in the dog park ')
+            remove_dog()
+
+        elif command == 'owner':
+            dog_owner()
